58df2c322a67c78741dac80ff1358abdbd819043
chibitofu <erinmoony@gmail.com>
2023-12-06 11:57:20 -0800

    Part 2 for day 2 complete.

--- day_02/day_02.js
+++ day_02/day_02.js
@@ -43,24 +43,25 @@ function computeValidGames(fileName) {
     let idSum = 0;
 
     for (let i = 0; i < games.length; i++) {
-        if (parseGame(games[i]) ) {
-            idSum += parseGame(games[i])
+        const isValid = validGameID(games[i])
+
+        if (isValid) {
+            idSum += isValid;
         }
     }
 
     return idSum;
 }
 
-function parseGame(game){
+//returns false or a number
+function validGameID(game){
     const gameID = getGameID(game);
     const setCollection = parseSets(game);
     let gameObject = {};
     gameObject["id"] = Number(gameID);
-    gameObject["sets"] = [];
 
     for (let i = 0; i < setCollection.length; i++) {
         let currentSet = setCollection[i].split(",");
-        gameObject["sets"].push([]);
 
         for (let j = 0; j < currentSet.length; j++) {
             const cubes = currentSet[j];
@@ -114,4 +115,69 @@ function checkScore(color, count){
     return true;
 };
 
-console.log(computeValidGames('input.txt'));
+/*
+Part 2
+For each game, find the minimum set of cubes that must have been present.
+Find the power of each minimum set of cubes.
+The power of a set of cubes is equal to the numbers of red, green, and blue cubes multiplied together.
+
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+
+The power of the minimum set of cubes in game 1 is 48 (4 red * 2 green * 6 blue).
+In games 2-5 it was 12, 1560, 630, and 36, respectively.
+Adding up these five powers produces the sum 2286.
+
+What is the sum of the power of these sets?
+*/
+function computeMinColors(fileName) {
+    const games = getData(fileName).split('\n');
+    let count = 0;
+
+    for (let i = 0; i < games.length; i++) {
+        const currentSet = minColors(games[i])
+        const reds = currentSet["red"];
+        const greens = currentSet["green"];
+        const blues = currentSet["blue"];
+        const colorSum = reds * greens * blues;
+
+        //console.log(reds)
+        count += colorSum;
+    }
+
+    return count;
+}
+
+function minColors(game){
+    const gameID = getGameID(game);
+    const setCollection = parseSets(game);
+    let gameObject = {};
+    gameObject["id"] = Number(gameID);
+    gameObject["colors"] = {"red":0, "green":0, "blue":0};
+
+    for (let i = 0; i < setCollection.length; i++) {
+        let currentSet = setCollection[i].split(",");
+
+        for (let j = 0; j < currentSet.length; j++) {
+            const cubes = currentSet[j];
+            //matches all letters after a space
+            const regexColor = /\S([a-z]+)/gi;
+            //matches all numbers
+            const regexCount = /[\d]+/g;
+            const color = String(cubes.match(regexColor)[0]);
+            const count = Number(cubes.match(regexCount)[0]);
+
+            if (gameObject["colors"][color] < count) {
+                gameObject["colors"][color] = count;
+            }
+        }
+    }
+
+    return gameObject["colors"];
+};
+
+console.log("game id sum:", computeValidGames('input.txt'));
+console.log("min set power:", computeMinColors('input.txt'));
